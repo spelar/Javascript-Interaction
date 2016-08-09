be5c9bbebd5c4c8ad91960f329225e805b1a45c1
Sungsu Kim <spelar7@gmail.com>
2016-08-09 13:44:46 +0900

    AJAX call하여 받은 데이터를 템플릿에 보여주는 코드 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,38 @@
 'use strict';
+
 require('jquery');
 require('underscore');
+
+
+function DataLoad(url, num){
+  this.init(url, num);
+}
+
+DataLoad.prototype.init = function(dataUrl, offset){
+  var objThis = this;
+  $.ajax({
+    method: "GET",
+    url: dataUrl + offset,
+    dataType: "jsonp"
+  }).then(function( result ) {
+    _.each(result.results, function(itemData){
+      objThis.render(itemData);
+    })
+  });
+}
+
+DataLoad.prototype.render = function(data){
+  $(".contents-wrap").append(this.itemTempleat(data));
+}
+
+DataLoad.prototype.itemTempleat = _.template(
+'<tr>'+
+  '<td><img src="<%= picture.large %>"/></td>'+
+  '<td><%= gender %></td>'+
+  '<td><%= name.last %> <%= name.first %></td>'+
+  '<td><%= email %></td>'+
+  '<td><%= phone %></td>'+
+'</tr>');
+
+var randomUserApp = new DataLoad("http://api.randomuser.me/?results=", 3);
+console.dir(randomUserApp);
--- src/js/bundle/app.js
+++ src/js/bundle/app.js
@@ -1,7 +1,42 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
+
 require('jquery');
 require('underscore');
+
+
+function DataLoad(url, num){
+  this.init(url, num);
+}
+
+DataLoad.prototype.init = function(dataUrl, offset){
+  var objThis = this;
+  $.ajax({
+    method: "GET",
+    url: dataUrl + offset,
+    dataType: "jsonp"
+  }).then(function( result ) {
+    _.each(result.results, function(itemData){
+      objThis.render(itemData);
+    })
+  });
+}
+
+DataLoad.prototype.render = function(data){
+  $(".contents-wrap").append(this.itemTempleat(data));
+}
+
+DataLoad.prototype.itemTempleat = _.template(
+'<tr>'+
+  '<td><img src="<%= picture.large %>"/></td>'+
+  '<td><%= gender %></td>'+
+  '<td><%= name.last %> <%= name.first %></td>'+
+  '<td><%= email %></td>'+
+  '<td><%= phone %></td>'+
+'</tr>');
+
+var randomUserApp = new DataLoad("http://api.randomuser.me/?results=", 3);
+console.dir(randomUserApp);
 
 },{"jquery":2,"underscore":3}],2:[function(require,module,exports){
 (function (global){
